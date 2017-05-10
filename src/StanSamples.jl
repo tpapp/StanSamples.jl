0cff02291b6e86e84d4263cf33a24c4f2717eb87
Tamas K. Papp <tkpapp@gmail.com>
2017-05-10 20:05:01 +0200

    Wrote header parsing utilities.

--- src/StanSamples.jl
+++ src/StanSamples.jl
@@ -1,5 +1,66 @@
 module StanSamples
 
-# package code goes here
+using ArgCheck
+using AutoHashEquals
+
+iscommentline(s::String) = ismatch(r"^ *#", s)
+
+fields(s::String) = split(s, ',')
+
+abstract type HeaderVar end
+
+@auto_hash_equals struct ScalarVar <: HeaderVar
+    name::Symbol
+end
+
+@auto_hash_equals struct IndexedVar{N} <: HeaderVar
+    name::Symbol
+    index::CartesianIndex{N}
+end
+
+IndexedVar(name::Symbol, index::Int...) = IndexedVar(name, CartesianIndex(tuple(index...)))
+
+function parse_varname(s::String)
+    s = split(s, ".")
+    name = Symbol(s[1])
+    if length(s) > 1
+        indexes = parse.(Int, s[2:end])
+        @argcheck all(indexes .≥ 1) "Non-positive index in $(s)."
+        IndexedVar(name, indexes...)
+    else
+        ScalarVar(name)
+    end
+end
+
+combine_vars(vars) = _combine_vars(vars[1], vars)
+
+_combine_vars(var::ScalarVar, vars) = var, 1
+
+function combined_size(indexes)
+    siz = reduce(max, indexes)
+    ran = CartesianRange(siz)
+    # FIXME inelegant collect below
+    @argcheck collect(indexes) == vec(collect(ran)) "Non-contiguous indexes."
+    siz
+end
+
+function _combine_vars{N}(var::IndexedVar{N}, vars)
+    name = var.name
+    len = findfirst(v -> v.name != name, vars)
+    len = len == 0 ? length(vars) : len-1
+    IndexedVar(name, combined_size(v.index for v in vars[1:len])), len
+end
+
+function parse_header(vars)
+    header = HeaderVar[]
+    position = 1
+    while position ≤ length(vars)
+        v, len = combine_vars(@view vars[position:end])
+        @argcheck v.name ∉ (h.name for h in header) "Duplicate variable $(v.name)."
+        position += len
+        push!(header, v)
+    end
+    header
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,50 @@
 using StanSamples
+import StanSamples:
+    iscommentline,
+    fields,
+    ScalarVar,
+    IndexedVar,
+    parse_varname,
+    combined_size,
+    parse_header
 using Base.Test
 
+@testset "raw reading" begin
+    @test iscommentline("# this is a comment")
+    @test iscommentline(" # this is a comment")
+    @test iscommentline("#")
+    @test !iscommentline("fly in the ointment #")
+    @test !iscommentline("99,12")
+    @test !iscommentline("99,12#comment")
+    @test fields("a,b,c") == ["a","b","c"]
+    @test fields("") == [""] # corner cases, should not appear in a CSV produced by CmdStan
+    @test fields(",") == ["",""]
+end
+
+@testset "parsing variable names" begin
+    @test parse_varname("a") == ScalarVar(:a)
+    @test parse_varname("b99") == ScalarVar(:b99)
+    @test parse_varname("accept_stat__") == ScalarVar(:accept_stat__)
+    @test parse_varname("a.1.2.3") == IndexedVar(:a, 1, 2, 3)
+    @test_throws ArgumentError parse_varname("a.foo")
+    @test_throws ArgumentError parse_varname("a.0")
+    @test_throws ArgumentError parse_varname("a.0.")
+end
+
+@testset "combined size" begin
+    @test combined_size([CartesianIndex((i,)) for i in 1:3]) == CartesianIndex((3,))
+    @test combined_size(vec([CartesianIndex((i,j)) for i in 1:3, j in 1:4])) ==
+        CartesianIndex((3,4))
+    @test_throws ArgumentError combined_size([CartesianIndex((i,)) for i in [1,3]])
+    @test_throws ArgumentError combined_size([CartesianIndex((i,)) for i in [2,1]])
+end
+
+@testset "parsing header" begin
+    let h = ScalarVar.([:a, :b, :c])
+        @test parse_header(h) == h
+    end
+    @test parse_header(parse_varname.(["a", "b.1", "b.2", "c"])) ==
+        [ScalarVar(:a), IndexedVar(:b, 2), ScalarVar(:c)]
+    @test parse_header(parse_varname.(["a", "b.1.1", "b.2.1", "b.1.2", "b.2.2", "c"])) ==
+        [ScalarVar(:a), IndexedVar(:b, 2, 2), ScalarVar(:c)]
+end
